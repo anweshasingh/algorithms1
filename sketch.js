69b589d0629339eb1245168035b14022708d4a87
anweshasingh <72261344+anweshasingh@users.noreply.github.com>
2020-10-13 18:47:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var fr,mr;
+
+function setup() {
+  createCanvas(800,400);
+ 
+  fr = createSprite(200,200,50,50);
+  fr.shapeColor = "green";
+  fr.velocityX = 2;
+
+  mr = createSprite(400,200,50,50);
+  mr.shapeColor = "green";
+  mr.velocityX = -2;
+}
+
+function draw() {
+  background(255,255,255);  
+
+  //mr.x = World.mouseX;
+  //mr.y = World.mouseY;
+  if(mr.x-fr.x<=mr.width/2+fr.width/2 && 
+    fr.x - mr.x<=mr.width/2+fr.width/2 &&
+    mr.y - fr.y<=mr.height/2+fr.height/2 &&
+    fr.y-mr.y<=mr.height/2+fr.height/2){
+    
+      //fr.shapeColor = "red";
+      //mr.shapeColor = "red";
+      mr.velocityX = mr.velocityX*(-1);
+      fr.velocityX = fr.velocityX*(-1);
+  }
+  else{
+    //fr.shapeColor = "green";
+   //mr.shapeColor = "green";
+   mr.velocityX = mr.velocityX;
+   fr.velocityX = fr.velocityX;
+  }
+  drawSprites();
+}
